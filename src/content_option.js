8837047d515321fd11ff2fffbc36424751860112
MarciiKai <kai.marcii2000@gmail.com>
2023-09-22 19:28:07 +0300

    My awesome portfolio

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "KAI";
+const meta = {
+    title: "Mercy Kai",
+    description: "I’m Mercy Kai  _ Front-end devloper and Mobile Developer",
+};
+
+const introdata = {
+    title: "Hey, It's Mercy Kai",
+    animated: {
+        first: "I love coding",
+        second: "I develop cool websites",
+        third: "I develop mobile apps",
+        fourth: "I am a cybersecurity enthusiast"
+    },
+    description: "As a Junior Software Developer and Cybersecurity Enthusiast, I have a passion for building secure and efficient software solutions. In the past, I have gained experience in software development through personal projects. I have worked on developing web applications and mobile apps. This has allowed me to develop a strong foundation in front-end languages include HTML, CSS and JavaScript, Scripting language include Php, Progamming Languages Kotlin and SQL & NoSQL as well as frameworks like Bootsrap and libraries like React JS.",
+    your_img_url: "https://lh3.googleusercontent.com/pw/AIL4fc-ezmCQpLl4BMa8eiHCqyq1Wv-iT4cUybd0pWgAuFX0K7imola5jmYJrGoqYq3K19dlGGFk-bHClbBqrmWgROiD05DjjNtXmoWncS2DkV1Dqvk5SVu7drsiZBcNa5YmPdcoFO3S-QMEj8Uv1hrlKPijgtpeH-FwR5bFQvZ07oiVJZm6YlAmIV-DuG27IU4EL8EkcXlcm3Xvx7-ve5j8-AhUErYuTvYZvm9gKIPBmEyWCCnPOKJDVOCEAD7JN1C5GWnHt5I4hjXti6R0WW9slIvNZ1NbhzN9Amj37mylF38g5xiKldY_NAUbVi4vVHo8LhG9bl4-lZOUkbJRKXXRrgTO8lSMI4ABl7Em60i7G3IGXb0HhWChRXk_T2tgfJVtrAdIr3hunOWWgjfzuhRr33kUt-P8i0ggHFY9ryA4AxVzR_i3cSm2JQJnjDlw8df7lzAOAY8DiuKdkbxYa1YdniiQJVknco7Ee7Zfc0u2sKRoVDTBnHlHfYDxjbyBOmf3vxl1JZnyPd3jdZCF0D58LRQTKqafMQfZDmRm4ZWVTOJfHDCHl9DMP4Ya4x9-G4F1XL6nnbVzhlkcIIfHITAcDpQZClBQdUlg-gKJaecnI4lDrjbXjlbRcU60ARG2-hQxIuRKO4vTL7nDcqVwTqlShMQaFtirtXfTUB1v7LcwI3-pVvs4ISXjxHlklAgN4I0a3uHcUe3YQ-GvawQXItwOc8o7iTZd1su7ase7Oxg4MOnhnaHoLKtVaJvL7a0cdZxOpy2xFKG5325ioBn2hraPBUIfDKHGFxvuO8Wra9vvYKD52DCnDJVgJfjq5OOSkghaVmUKEWxohXeR33EGwFD9TVJVUwQ_LKoWwHg5EEZSwicNKBibbNX4GQUePLcd8MezWaxDQPtcowmmHKI0RepGBQ=w575-h640-s-no?authuser=0",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "I am a passionate and driven individual with a strong foundation in software development and a deep enthusiasm for cybersecurity. My journey in the world of technology began with personal projects, where I honed my skills in web and mobile app development.",
+};
+const worktimeline = [{
+        jobtitle: "IT Intern",
+        where: "Rai Plywoods",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "Tailwind",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Kotlin",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kai.marcii2000@gmail.com",
+    YOUR_FONE: "(+254) 797346811",
+    description: "Junior Software Developer",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MarciiKai",
+    linkedin: "https://www.linkedin.com/in/mercy-kai-a62121257/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
